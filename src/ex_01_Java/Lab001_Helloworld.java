81b13e2a04d2387f475e05aad23fcd24a0409e2d
Nikhilendra <nikhilendrapns@gmail.com>
2025-09-07 17:54:26 +0530

    Updating first program

--- src/ex_01_Java/Lab001_Helloworld.java
+++ src/ex_01_Java/Lab001_Helloworld.java
@@ -1,4 +1,9 @@
 package ex_01_Java;
 
 public class Lab001_Helloworld {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World..!, Welcome to my Java programming journey..!");
+    }
 }
