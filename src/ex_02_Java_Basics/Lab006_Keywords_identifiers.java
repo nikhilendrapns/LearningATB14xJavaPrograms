b35d4c77879d0e1343007f2e2ce74d1a60f1aae2
Nikhilendra <nikhilendrapns@gmail.com>
2025-09-20 16:30:56 +0530

    java basics Keywords code commit

---
+++ src/ex_02_Java_Basics/Lab006_Keywords_identifiers.java
@@ -0,0 +1,32 @@
+package ex_02_Java_Basics;
+
+public class Lab006_Keywords_identifiers {
+
+        public static void main(String[] args) {
+
+            /*
+            **Keywords** are **predefined**, **reserved words in Java** that have special meanings and functions within the language. They are used to define the structure and behavior of Java programs.
+
+**Keywords:**
+
+- int, class, public, static, void, if, else, while, for
+
+
+**Identifiers**, on the other hand, are names created by programmers to identify various program elements such as variables, methods, classes, and interfaces
+
+
+
+**Identifiers:**
+
+- score, highestScore, convertToString, number1
+
+
+#### How many Java keywords are currently used?
+53-56 keywords
+
+
+             */
+            System.out.println("Hi,Nikhilendra");
+        }
+    }
+
