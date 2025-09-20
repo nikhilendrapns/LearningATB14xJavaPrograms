e8315f1f4d983ba4c7038b4305806ff43f6c31c5
Nikhilendra <nikhilendrapns@gmail.com>
2025-09-20 16:30:08 +0530

    java basics code commit

--- src/ex_02_Java_Basics/Lab005_Multi_line_comments.java
+++ src/ex_02_Java_Basics/Lab005_Multi_line_comments.java
@@ -2,7 +2,7 @@ package ex_02_Java_Basics;
 
 public class Lab005_Multi_line_comments {
     public static void main(String[] args) {
-                   /**
+                   /*
                     * *
                     * This syntax is specifically designated as a Javadoc comment, a special type of block comment used to generate API documentation.
                     *  While the Java compiler ignores these comments just like any other block comment, the javadoc tool included with the JDK processes them to create formal documentation,
